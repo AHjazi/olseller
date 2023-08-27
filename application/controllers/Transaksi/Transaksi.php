d31a69af35e8725e5c9c4ba7c0a761553db31b3e
AHjazi <ahmdhjzi06@gmail.com>
2023-08-27 10:53:16 +0800

    berhasil menampilkan data transaksi dari database transaksi

--- application/controllers/Transaksi/Transaksi.php
+++ application/controllers/Transaksi/Transaksi.php
@@ -5,9 +5,11 @@ class Transaksi extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->model('M_Transaksi');
+		$data['transaksi'] = $this->M_Transaksi->show_data();
 		$this->load->view('partials_admin/header',);
         $this->load->view('partials_admin/sidebar');
-        $this->load->view('Transaksi/transaksi');
+        $this->load->view('Transaksi/transaksi',$data);
         $this->load->view('partials_admin/footer');
 	}
 }
